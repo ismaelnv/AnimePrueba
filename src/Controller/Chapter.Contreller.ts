1604f185d4d5c0f629349db6d62b8e77c3f952b4
ismaelnv <jonasvillarruelnegrete@gmail.com>
2023-09-08 15:35:15 -0500

    Metodo getChapters

--- src/Controller/Chapter.Contreller.ts
+++ src/Controller/Chapter.Contreller.ts
@@ -24,12 +24,16 @@ export const createChapter = async (req: Request, res: Response) => {
 export const getChapters = async (req: Request, res: Response) => {
     try {
         const charpter = await Chapter.find({ relations: { anime: true } });
-        console.log(charpter);
-        return res.json(charpter);
+        if(charpter == null){
+            return res.status(404).json({message: "No chapter found"});
+        }
+        return res.status(200).json(charpter);
     } catch (error) {
         if (error instanceof Error) {
             return res.status(500).json({ message: error.message });
         }
+        console.log(error);
+        return res.status(500).json({ message: "Error when fetching the chapters" });
     }
 };
 
